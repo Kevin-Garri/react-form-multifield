e6abf5c746ca25fe75e8b9ec3199654f54f4c850
Kevin-Garri <kevin.alexander.garri29@gmail.com>
2025-01-21 15:45:31 +0100

    fit: array di categorie e tag, dati iniziali (initialFormData), gli stati(formData e posts), modifica del modulo, rimozione dell'elemento

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,73 +1,54 @@
 import React, { useState } from 'react';
 
-const list = []
-//titolo articolo e lista articoli
+const categorie = ["Tecnologia", "Salute", "Sport", "Cultura"];
+
+const tags = [
+  { id: 1, nome: "Javascript" },
+  { id: 2, nome: "React" },
+  { id: 3, nome: "CSS" },
+  { id: 4, nome: "HTML" },
+  { id: 5, nome: "Node" },
+];
+
+//initial form data è oggetto con tutte le proprietà
 const Form = () => {
-  const [title, setTitle] = useState(list);
-  const [articles, setArticles] = useState({
-    "text": "",
-    "id": "",
-    "titolo": "",
-    "contenuto": "",
-    "categoria": "",
-    "immagini": "",
-    "tags": [],
-    "stato": ""
-  });
-
-  //funzione submit
-  const handlerSubmit = (e) => {
-    e.preventDefault();
-    setTitle([articles, ...title]);
+  const initialFormData = {
+    id: "",
+    titolo: "",
+    categoria: "",
+    immagini: "",
+    tags: [],
+    stato: "",
   };
-  const handlerArticles = (e) => {
-    const articles = {
-      id: Date.now(),
-      text: e.target.value,
+  //post variabile setpost funzione
+  const [formData, setFormData] = useState({ initialFormData });
+  const [posts, setPosts] = useState([]);
+
+  const handleField = (e) => {
+    let value = e.target.value;
+    if (e.target.type === "checkbox") {
+      value = e.target.checked;
+    }
+    setFormData({
+      ...formData,
+      [e.target.name]: value,
+    });
+  };
+
+  const handlerNewTask = (e) => {
+    const newArticle = {
+      ...formData,
+      [e.target.name]: e.target.value,
     };
-    setArticles(articles);
-  };
-  const handlerRemove = (id) => {
-    const newTitle = title.filter((item) => item.id !== id);
-    setTitle(newTitle);
+    setFormData(newArticle);
   };
 
-  return (
-    <>
-      <div className="container my-5">
-        <form action="#" onSubmit={handlerSubmit}>
-          <div className="input-group">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Scrivi titolo da aggiungere..."
-              value={articles.text}
-              onChange={handlerArticles}
-            />
-            <button className="btn btn-outline-dark bg-success" type="submit">
-              Aggiungi
-            </button>
-          </div>
-        </form>
-      </div>
-      <div className="container">
-        <ul className="list-group">
-          {title.map((title) => (
-            <li
-              key={title.id}
-              className="list-group-item  d-flex justify-content-between"
-            >
-              {title.text}
-              <i
-                className="fa-solid fa-trash"
-                onClick={() => handlerRemove(title.id)}
-              ></i>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
-  );
+  const handlerRemove = (id) => {
+    const newList = myList.filter((item) => item.id !== id);
+    setMylist(newList);
+  };
+
+  return
 };
 
 export default Form;
